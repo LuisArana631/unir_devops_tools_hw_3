2b1bf246c77bafd939e800876d99d81fe3177da6
Bloc De Notas <luarana631@gmail.com>
2025-02-02 20:37:16 -0600

    chore: add private ip output

--- terraform/mongodb/outputs.tf
+++ terraform/mongodb/outputs.tf
@@ -1,3 +1,7 @@
 output "mongodb_public_ip" {
   value = aws_instance.mongodb.public_ip
 }
+
+output "mongodb_private_ip" {
+  value = aws_instance.mongodb.mongodb_private_ip
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,9 @@ output "public_ips" {
     "MongoDB IP" = module.mongodb.mongodb_public_ip
   }
 }
+
+output "private_ips" {
+  value = {
+    "MongoDB IP" = module.mongodb.mongodb_private_ip
+  }
+}
